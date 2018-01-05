64432857db58d054694e7a5c323dc6657a0fe159
kcarretto <kcarretto@gmail.com>
2018-01-05 11:04:19 -0500

    Reformatted. Updated koth status URI

--- app/api.py
+++ app/api.py
@@ -2,21 +2,16 @@
 from . import app
 from flask import request, jsonify
 
-def validateteamID(team_id):
-    if team_id is None:
-        return False
-    return True
-
-@app.route('/ships/<teamID>')
-def getShips(teamID):
-    if not validateteamID(teamID):
-        return jsonify({'status': 404})
-    return jsonify({'status': 200, 'ship1_count': 50, 'ship2_count': 0, 'ship3_count': 1000})
-
-@app.route('/koth')
+"""
+    Statuses
+"""
+@app.route('/status/koth')
 def koth():
     return jsonify({'status': 200, 'planets': {'planet1': 'team4'}})
 
+"""
+        Team Functions
+"""
 @app.route('/credits/<teamID>')
 def getCredits(teamID):
     if not validateteamID(teamID):
@@ -29,6 +24,16 @@ def getPerks(teamID):
         return jsonify({'status': 404})
     return jsonify({'status': 200, 'active_perks': ['perk1', 'perk2', 'bonus health']})
 
-@app.route('/resources')
-def getResources():
-    return jsonify({'status': 200, 'resources': {'resource1': 1000, 'resource2': 2000}})
+@app.route('/ships/<teamID>')
+def getShips(teamID):
+    if not validateteamID(teamID):
+        return jsonify({'status': 404})
+    return jsonify({'status': 200, 'ship1_count': 50, 'ship2_count': 0, 'ship3_count': 1000})
+
+"""
+        Helpers
+"""
+def validateteamID(team_id):
+    if team_id is None:
+        return False
+    return True
